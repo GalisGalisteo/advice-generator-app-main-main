70f43520760843edfbb71dc2bbfca28968afb81b
GalisGalisteo <galis.travelling@gmail.com>
2023-10-31 09:50:53 +0100

    finishing ex

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-// ¡Buena suerte!
+const fetchAdvice = async () => {
+    const response = await fetch('https://api.adviceslip.com/advice');
+    if (!response.ok) {
+        throw new Error('Error fetching advice');
+    }
+    const data = await response.json();
+    return data;
+}
+
+const diceButton = document.querySelector('#dice-button');
+diceButton.addEventListener('click', async () => {
+    const adviceId = document.querySelector('#advice-id');
+    const adviceText = document.querySelector('#advice-text');
+    try {
+        const advice = await fetchAdvice();
+        adviceId.textContent = advice.slip.id;
+        adviceText.textContent = advice.slip.advice;
+    } catch (error){
+        adviceId.textContent = 'Error'
+        adviceText.textContent = "Ups! We can't retrieve the data, try again later!"
+        console.error(error);
+    }
+})
